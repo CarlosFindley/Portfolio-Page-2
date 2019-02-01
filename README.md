47d68ac7190167de52732b09e5151d6fe7b5d8a9
Carlos Villalta <CarlosFindley@protonmail.com>
2019-01-31 22:01:07 -0800

    updating portfolio

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Portfolio-Page-2
+
+Currently deployed in GitHub: https://carlosfindley.github.io/Carlos-Portfolio/
+
+To be deployed in Heroku
---
+++ resources/scripts/slickFeature.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+
+    $(".slideshow").slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 4000,
+      })
+      console.log("hi");
+
+  });
